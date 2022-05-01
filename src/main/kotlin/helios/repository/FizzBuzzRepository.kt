dc002ffe3a8a6bbdb99fe26ea048a2111d7c24cf
laura <laura.apremont@hotmail.fr>
2022-05-01 16:14:57 +0200

    fixe query to find the most used request of fizz buzz

--- src/main/kotlin/helios/repository/FizzBuzzRepository.kt
+++ src/main/kotlin/helios/repository/FizzBuzzRepository.kt
@@ -6,6 +6,28 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface FizzBuzzRepository : CrudRepository<FizzBuzz, FizzBuzzId> {
-    @Query("SELECT * FROM fizz_buzz", nativeQuery = true)
+    @Query(
+        "SELECT * " +
+                "FROM fizz_buzz " +
+                "ORDER BY request_count DESC " +
+                "limit 1", nativeQuery = true
+    )
     fun findMostUsedRequest(): FizzBuzz?
+
+    @Query(
+        "SELECT * FROM fizz_buzz " +
+                "WHERE first_multiple = :firstMultiple " +
+                "AND second_multiple = :secondMultiple " +
+                "AND request_limit = :limit " +
+                "AND first_multiple_response = :firstMultipleResponse " +
+                "AND second_multiple_response = :secondMultipleResponse",
+        nativeQuery = true
+    )
+    fun findByRequestsField(
+        firstMultiple: Int,
+        secondMultiple: Int,
+        limit: Int,
+        firstMultipleResponse: String,
+        secondMultipleResponse: String
+    ): FizzBuzz?
 }
