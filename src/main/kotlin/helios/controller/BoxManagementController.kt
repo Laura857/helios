44a3d95755bddb0dd98a4b75582d44fb988a37f6
laura <laura.apremont@hotmail.fr>
2022-05-03 19:39:21 +0200

    implements the api documentation

--- src/main/kotlin/helios/controller/BoxManagementController.kt
+++ src/main/kotlin/helios/controller/BoxManagementController.kt
@@ -1,6 +1,8 @@
 package helios.controller
 
 import helios.service.BoxManagementService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +15,16 @@ class BoxManagementController {
     lateinit var boxManagementService: BoxManagementService
 
     @GetMapping("api/v1/itemsPerBox/{itemsSize}")
-    fun findBestItemsNumberPerBox(@PathVariable("itemsSize") itemsSize: String) =
+    @Operation(
+        summary = "Optimize the number of items per box in order to maximize the number of boxes"
+    )
+    fun findBestItemsNumberPerBox(
+        @PathVariable("itemsSize")
+        @Schema(
+            example = "1",
+            description = "A string with items size"
+        )
+        itemsSize: String
+    ) =
         boxManagementService.findBestItemsNumberPerBox(itemsSize)
 }
--- src/main/kotlin/helios/controller/FizzBuzzController.kt
+++ src/main/kotlin/helios/controller/FizzBuzzController.kt
@@ -2,6 +2,8 @@ package helios.controller
 
 import helios.dto.FizzBuzzRequest
 import helios.service.FizzBuzzService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -13,15 +15,24 @@ class FizzBuzzController {
     lateinit var fizzBuzService: FizzBuzzService
 
     @GetMapping("api/v1/fizzBuzz")
+    @Operation(
+        summary = "Return all number from 1 to limit by replacing all multiple of int1 by str1 " +
+                "and all multiple of int2 by str2 " +
+                "and all multiple of int1 and int2 by str1str2"
+    )
     fun fizzBuzz(
-        @RequestParam("int1") int1: Int,
-        @RequestParam("int2") int2: Int,
-        @RequestParam("limit") limit: Int,
-        @RequestParam("str1") str1: String,
-        @RequestParam("str2") str2: String
+        @RequestParam("int1") @Schema(example = "3") int1: Int,
+        @RequestParam("int2") @Schema(example = "5") int2: Int,
+        @RequestParam("limit") @Schema(example = "100") limit: Int,
+        @RequestParam("str1") @Schema(example = "Fizz") str1: String,
+        @RequestParam("str2") @Schema(example = "Buzz") str2: String
     ) =
         fizzBuzService.fizzBuzz(FizzBuzzRequest(int1, int2, limit, str1, str2))
 
     @GetMapping("api/v1/fizzBuzz/statistics")
+    @Operation(
+        summary = "Return the parameters corresponding to the most frequent request " +
+                "and the number of hits for this request"
+    )
     fun fizzBuzzStatistics() = fizzBuzService.fizzBuzzStatistics()
 }
